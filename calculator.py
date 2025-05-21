156278c141b20604f54fe86a2ee1c1cf9b42b96e
Reyansh-Coder <mr.reyanshgarg2012@gmail.com>
2025-05-21 18:48:31 +0530

    i

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Input base and exponent from the user
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the exponent (power): "))
+
+# Calculate power
+result = base ** exponent
+
+# Display result
+print(f"\n{base} raised to the power of {exponent} is: {result}")
